daa285a22681e754d85587bc9dd4a24c2dd1e4a6
sergiotto <sergiotto@outlook.com>
2023-03-13 17:20:48 +0100

    primer commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hola</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
